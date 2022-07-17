337a3011e5763313c12f18b81634508dc4044977
vahagnmovs <vahag999988@gmail.com>
2022-07-17 20:32:47 +0400

    TASK 42 find epmloyee with inputing name

---
+++ 42.find-epmloyee-with-name.js
@@ -0,0 +1,8 @@
+// TASK 42
+// Create a function that will find the employee by the provided name
+
+// function findEmployeeWithName(array, name = '') {
+//     return array.find(person => person.firstName === name)
+// }
+//
+// console.log(findEmployeeWithName(employees, 'Arabela'))
