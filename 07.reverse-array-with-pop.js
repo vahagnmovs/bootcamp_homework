587a8b82a3f1ea3a923e2c3e50a1977a071d28d6
vahagnmovs <vahag999988@gmail.com>
2022-07-17 19:15:47 +0400

    TASK 8 reveresing array with pop method

---
+++ 07.reverse-array-with-pop.js
@@ -0,0 +1,14 @@
+// TASK 7
+// Create a function that is receiving an array and returns reversed one using pop()
+
+function reverseWithPop(array) {
+    let tempArray = [...array];
+    let result = [];
+    for (let idx = 0; idx < array.length; idx++) {
+        result.push(tempArray.pop())
+    }
+    return result
+}
+
+const array = [1, 2, 3, 4, 5]
+console.log(reverseWithPop(array))
