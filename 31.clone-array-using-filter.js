ce1187779667e18106d0c5fef7689baf852214c5
vahagnmovs <vahag999988@gmail.com>
2022-07-17 19:51:53 +0400

    TASK 31 cloning array with filter

---
+++ 31.clone-array-using-filter.js
@@ -0,0 +1,9 @@
+// TASK 31
+// Using the filter, write a JavaScript function to clone an array
+
+function cloneArraywithFilter(array) {
+    return array.filter(elem => elem)
+}
+
+let array = [1, 2, 3]
+console.log(cloneArraywithFilter(array))
